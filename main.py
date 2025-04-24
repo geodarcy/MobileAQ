6e09273640ea8654bd0fd9577f5990f6ed495f1e
Darcy <geodarcy@gmail.com>
2025-04-23 21:28:31 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import asyncio
+from airgradient import AirGradientClient
+
+async def main() -> None:
+    async with AirGradientClient('192.168.1.234') as client:
+        measurements = await client.get_current_measures()
+        print(measurements)
+
+if __name__ == "__main__":
+    asyncio.run(main())
